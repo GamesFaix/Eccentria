2e5bc5d9579b22e2daabc3ab06f07a28635bea26
James Faix <jamesthefaix@gmail.com>
2021-07-06 12:13:00 -0700

    Fixed image sizes

--- utils/numberer/Layouter.fs
+++ utils/numberer/Layouter.fs
@@ -12,7 +12,7 @@ let private getImageTag (card: CardInfo) : string =
     sprintf "<img src=\"%s\"/>" (FileReaderWriter.getCardFileName card)
 
 let createHtmlLayout (cards : CardInfo list) : string =
-    let styleTag = getStyleTag (3.5, 2.5)
+    let styleTag = getStyleTag (3.46875, 2.46875)
     let cardTags = cards |> List.map getImageTag
 
     [
